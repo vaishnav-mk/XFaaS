9d211b66c13e6ec270766851d40c71b25f98f0af
Tarun Pal <tanmaypal13982@gmail.com>
2024-04-08 14:29:04 +0530

    Finalizing result_analysis_script after discussion

--- result_analysis_script.py
+++ result_analysis_script.py
@@ -3,7 +3,6 @@ import pandas as pd
 import requests
 import networkx as nx
 import os
-from openpyxl import Workbook
 
 
 
@@ -80,92 +79,111 @@ def custom_dfs(graph, node, dataframe,result_list, node_to_nodeid,path="",ntime=
     
     return paths_with_weights
 
+class result_analysis:
+    def data_analysis(url_list):
+        for url,wf_path in url_list:
+            out,instance_id=creating_outobject(url)
+            df = pd.DataFrame.from_dict(out).set_index('NodeId')
+            dag_path=wf_path+'/refactored-dag.json'
+            with open(dag_path, 'r') as file:
+                data = json.load(file)
 
-def data_analysis(url_list):
-    for url,dag_path in url_list:
-        out,instance_id=creating_outobject(url)
-        df = pd.DataFrame.from_dict(out).set_index('NodeId')
-        
-        with open(dag_path, 'r') as file:
-            data = json.load(file)
+            # Create a directed graph
+            node_to_nodeid={}
+            nodeid_to_node={}
+            graph = nx.DiGraph()
 
-        # Create a directed graph
-        node_to_nodeid={}
-        nodeid_to_node={}
-        graph = nx.DiGraph()
+            # Add nodes to the graph
+            for node in data['Nodes']:
+                graph.add_node(node['NodeName'])
+                node_to_nodeid[node['NodeName']]=node['NodeId']
+                nodeid_to_node[node["NodeId"]]=node['NodeName']
 
-        # Add nodes to the graph
-        for node in data['Nodes']:
-            graph.add_node(node['NodeName'])
-            node_to_nodeid[node['NodeName']]=node['NodeId']
-            nodeid_to_node[node["NodeId"]]=node['NodeName']
+            # Add edges to the graph
+            for edge in data['Edges']:
+                for source, targets in edge.items():
+                    for target in targets:
+                        graph.add_edge(source, target)  # Assuming each edge has only one target
 
-        # Add edges to the graph
-        for edge in data['Edges']:
-            for source, targets in edge.items():
-                for target in targets:
-                    graph.add_edge(source, target)  # Assuming each edge has only one target
+            # Print the graph nodes and edges
+            # print("Nodes:", graph.nodes(data=False))
+            # print("Edges:", graph.edges())
+            # print("NodeMap:",node_to_nodeid)
+            workflow_name=data['WorkflowName']
+            result_list=[]
+            custom_dfs(graph, nodeid_to_node['1'], df,result_list,node_to_nodeid)
 
-        # Print the graph nodes and edges
-        # print("Nodes:", graph.nodes(data=False))
-        # print("Edges:", graph.edges())
-        # print("NodeMap:",node_to_nodeid)
-        workflow_name=data['WorkflowName']
-        result_list=[]
-        custom_dfs(graph, nodeid_to_node['1'], df,result_list,node_to_nodeid)
+            max_time=max_cost=0
+            critical_path=""
+            for (path,(time,cost)) in result_list:
+                if time>max_time:
+                    (critical_path,(max_time,max_cost))=(path,(time,cost))
+            max_time=max_time/1000
+            start_node_data=df.loc['1'].to_dict()
+            end_node_data=df.loc['253'].to_dict()
+            start_timestamp=start_node_data['start_delta']
+            end_timestamp=end_node_data['end_delta']
+            total_workflow_exec_time=(end_timestamp-start_timestamp)/1000
+            waiting_time=(total_workflow_exec_time-max_time)
+            total_cost=df['cost'].sum()
+            print('For workflow:\t', workflow_name,'\n\t InstanceId:\t',instance_id ,'\n\t\tTotal workflow execution time:\t',total_workflow_exec_time , 'seconds\n\t\tTotal funtion exec time(as per critical path):\t',max_time,'seconds\n\t\tTotal waiting time(as per critical path):\t',waiting_time,'seconds\n\t\tCritical Path:\t',critical_path,'\b\b\n\t\tTotal Cost: \t$ ',total_cost)
+            results={
+                "WorkFlowName":workflow_name,
+                "InstanceId":instance_id,
+                "Total_workflow_exec_time":total_workflow_exec_time,
+                "TotalFuntionExecutionTime":max_time,
+                "Total_waiting_time":waiting_time,
+                "CriticalPath":critical_path,
+                "TotalCost":total_cost,
+                "PathResults": []
+            }
+            
 
-        max_time=max_cost=0
-        critical_path=""
-        for (path,(time,cost)) in result_list:
-            if time>max_time:
-                (critical_path,(max_time,max_cost))=(path,(time,cost))
-        max_time=max_time/1000
-        start_node_data=df.loc['1'].to_dict()
-        end_node_data=df.loc['253'].to_dict()
-        start_timestamp=start_node_data['start_delta']
-        end_timestamp=end_node_data['end_delta']
-        total_workflow_exec_time=(end_timestamp-start_timestamp)/1000
-        waiting_time=(total_workflow_exec_time-max_time)
-        total_cost=df['cost'].sum()
-        print('For workflow:\t', workflow_name,'\n\t InstanceId:\t',instance_id ,'\n\t\tTotal workflow execution time:\t',total_workflow_exec_time , ' seconds\n\t\tTotal funtion exec time(as per critical path):\t',max_time,' seconds\n\t\tTotal waiting time(as per critical path):\t',waiting_time,' seconds\n\t\tCritical Path:\t',critical_path,'\b\b\n\t\tTotal Cost: \t$ ',total_cost)
-        
+            # print(result_list)
+            print("\n\nFor each path total execution time and total waiting time")
+            for (path,(time,cost)) in result_list:
+                time=time/1000
+                out={"Path":path,
+                     "Total_exec_time":time,
+                     "Total waiting time":total_workflow_exec_time-time}
+                print("Path:",path,'\tTotal exec time:',time,'sec\tTotal waiting time:',total_workflow_exec_time-time,"sec")
+                results["PathResults"].append(out)
 
-        # print(result_list)
-        print("\n\nFor each path total execution time and total waiting time")
-        for (path,(time,cost)) in result_list:
-            time=time/1000
-            print("Path:",path,'\tTotal exec time:',time,'sec\tTotal waiting time:',total_workflow_exec_time-time,"sec")
+            result_dir = wf_path + '/results'
+            if not os.path.exists(result_dir):
+                os.makedirs(result_dir)
+            json_file_path = os.path.join(result_dir, f'{instance_id}.json')
+            text_file_path = os.path.join(result_dir, f'{instance_id}.txt')
+            with open(json_file_path, 'w') as json_file:
+                json.dump(results, json_file, indent=4)
 
+            print(f"Results saved to {json_file_path}")
 
+            with open(text_file_path, 'w') as f:
+                f.write('For workflow:\t' + workflow_name + '\n\t InstanceId:\t' + instance_id + '\n\t\tTotal workflow execution time:\t' + str(total_workflow_exec_time) + ' seconds\n\t\tTotal funtion exec time(as per critical path):\t' + str(max_time) + ' seconds\n\t\tTotal waiting time(as per critical path):\t' + str(waiting_time) + ' seconds\n\t\tCritical Path:\t' + critical_path + '\b\b\n\t\tTotal Cost: \t$ ' + str(total_cost) + '\n\n\nFor each path total execution time and total waiting time\n')
+                for (path, (time, cost)) in result_list:
+                    time = time / 1000
+                    f.write("Path:" + str(path) + '\tTotal exec time:' + str(time) + ' sec\tTotal waiting time:' + str(total_workflow_exec_time - time) + " sec\n")
 
+            # print('\n\n')
+            # output_file = 'output.xlsx'
+            # sheet_name = instance_id[0:30]
+            # if not os.path.isfile(output_file):
+            # # Create a new workbook and save it if the file does not exist
+            #     wb = Workbook()
+            #     wb.save(output_file)
 
+            
+            # with pd.ExcelWriter(output_file, engine='openpyxl', mode='a') as writer:
+            #     # Check if the sheet name already exists in the workbook
+            #     if sheet_name in writer.book.sheetnames:
+            #         # Get the sheet by name and remove it
+            #         std = writer.book[sheet_name]
+            #         writer.book.remove(std)
+            #     # Write the DataFrame to the sheet
+            #     df.to_excel(writer, sheet_name=sheet_name, index=True)
 
-
-
-        print('\n\n')
-        output_file = 'output.xlsx'
-        sheet_name = instance_id[0:30]
-        if not os.path.isfile(output_file):
-        # Create a new workbook and save it if the file does not exist
-            wb = Workbook()
-            wb.save(output_file)
-
-        
-        with pd.ExcelWriter(output_file, engine='openpyxl', mode='a') as writer:
-            # Check if the sheet name already exists in the workbook
-            if sheet_name in writer.book.sheetnames:
-                # Get the sheet by name and remove it
-                std = writer.book[sheet_name]
-                writer.book.remove(std)
-            # Write the DataFrame to the sheet
-            df.to_excel(writer, sheet_name=sheet_name, index=True)
-
-    print("Output is been stored into the file output.xlsx sucessfully")
+        print("Results stored sucessfully.")
 
 
 
-#In the url list just pass url_list=[(http_trigger_url_path,refractor.json_path),...]
-url_list=[('https://xfaasibmparallelsimulator12480876.azurewebsites.net/runtime/webhooks/durabletask/instances/f93f6c87acd74da89a34eb1cbebc82a8?taskHub=xfaasIBMParallelSimulator12480876&connection=Storage&code=uJleO1hkph_cDLcNLpD7U-aZzQg5Y96woPUqL1f9O4FRAzFu4FIBFw==','serwo/examples/ParallelSimulator12/refactored-dag.json'),('https://xfaasibmparallelsimulator12480876.azurewebsites.net/runtime/webhooks/durabletask/instances/80b259f3b67f418589c481e6e2bb7919?taskHub=xfaasIBMParallelSimulator12480876&connection=Storage&code=uJleO1hkph_cDLcNLpD7U-aZzQg5Y96woPUqL1f9O4FRAzFu4FIBFw==','serwo/examples/ParallelSimulator12/refactored-dag.json'),('https://xfaasibmparallelsimulator12480876.azurewebsites.net/runtime/webhooks/durabletask/instances/600788a5786f4dd79ee4e8e2c9eb6726?taskHub=xfaasIBMParallelSimulator12480876&connection=Storage&code=uJleO1hkph_cDLcNLpD7U-aZzQg5Y96woPUqL1f9O4FRAzFu4FIBFw==','serwo/examples/ParallelSimulator12/refactored-dag.json')]
-
-
-data_analysis(url_list)
